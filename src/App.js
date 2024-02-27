20d7ae9be76e723cd65aeb5046ae6a8cde4e1bfa
Zineb Katim <zkatim1301@gmail.com>
2024-02-27 20:33:00 +0100

    deploy gh pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './App.css';
+import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from '../src/components/Header'
 import Home from '../src/pages/Home'
-import Footer from './components/Footer';
-import FicheProjet from './pages/FicheProjet';
+import Footer from './components/Footer'
+import FicheProjet from './pages/FicheProjet'
 import Apropos from './pages/Apropos'
 
 function App() {
   return (
     <BrowserRouter>
-  <Header />
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/Apropos" element={<Apropos />} />
-    <Route path="/FicheProjet/:id" element={<FicheProjet />} />
-    {/* <Route path="*" element={<Error />} /> */}
-    </Routes>
-  < Footer />
-  </BrowserRouter>
-
-  );
+      <Header />
+      <Routes>
+        <Route path="/portfolio-mes-projet" element={<Home />} />
+        <Route path="/Apropos" element={<Apropos />} />
+        <Route path="/FicheProjet/:id" element={<FicheProjet />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
